430c2a6ce285a09af2d9a48d5ddcb41b058235bf
britbolt <blattisaw@gmail.com>
2022-08-08 00:21:45 -0400

    connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0:27017/googlebooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  // useCreateIndex: true,
+  useCreateIndex: true,
   // useFindAndModify: false,
 });
 
