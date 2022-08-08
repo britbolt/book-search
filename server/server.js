27cfa67f342663d149b0b6954105998d7406ede6
britbolt <blattisaw@gmail.com>
2022-08-07 21:22:11 -0400

    update server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-
-const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
 
 const {typeDefs, resolvers} = require('./schemas');
 const { authMiddleware } = require('./utils/auth')
-const db = require('./config/connection');
+
 const routes = require('./routes');
 
 
