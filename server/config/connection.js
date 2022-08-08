fb80739a78c3acb835edd04114fbe668d702990c
britbolt <blattisaw@gmail.com>
2022-08-07 21:56:12 -0400

    update server

--- server/config/connection.js
+++ server/config/connection.js
@@ -8,3 +8,5 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0/googlebooks', {
 });
 
 module.exports = mongoose.connection;
+
+
--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const server = new ApolloServer({
 
 const app = express();
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
